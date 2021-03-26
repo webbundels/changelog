def19e121102522753ece375a23b69e3ddbe112e
Nick van Leeuwen <nickvanleeuwen@Nicks-MacBook-Pro-2.local>
2021-03-26 14:39:55 +0100

    Removed save button from index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,7 +18,6 @@
                 <a id="new_chapter_button" class="styled-button" href="{{ route('changelog.create') }}">Nieuw hoofdstuk</a>
 
                 <div class="styled-button cancel" data-cancel-button>Annuleren</div>
-                <input class="styled-button save" data-save-button type="submit" value="Opslaan">
             </div>
         @endif
 
